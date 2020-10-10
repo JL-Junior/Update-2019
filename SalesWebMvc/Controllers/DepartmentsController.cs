7c9fdcd76d0e434dfd083385ddae6881a6a370a4
Jair <lopezz_lopez@hotmail.com>
2020-10-09 21:03:08 -0300

    Firs Model-View-Controller - Department

---
+++ SalesWebMvc/Controllers/DepartmentsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SalesWebMvc.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SalesWebMvc.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+
+
+
+            List<Department> listaDepto = new List<Department>();
+            listaDepto.Add(new Department { Id = 1, Name = "Electronics" });
+            listaDepto.Add(new Department { Id = 2, Name = "Fashion" });
+
+            return View(listaDepto);
+        }
+    }
+}
